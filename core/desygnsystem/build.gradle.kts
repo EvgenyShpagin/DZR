b6ab4f79e1a07995b0b242dd9ab8162d42233e55
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-11 18:37:37 +0700

    feat: create designsystem module

---
+++ core/desygnsystem/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.music.core.desygnsystem"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.runtime)
+    api(libs.androidx.ui.text.google.fonts)
+    api(libs.androidx.compose.material3)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ core/desygnsystem/src/main/java/com/music/dzr/core/desygnsystem/theme/Type.kt
@@ -0,0 +1,47 @@
+package com.music.dzr.core.desygnsystem.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.music.core.desygnsystem.R
+
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+private val fontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Oswald"),
+        fontProvider = provider,
+    )
+)
+
+private val baseline = Typography()
+
+internal val DzrTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    displayMedium = baseline.displayMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium
+    ),
+    displaySmall = baseline.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = fontFamily),
+)
