8f0b45f2ec1aa3ac8fa67debf613117ae0bb099d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-13 10:50:24 +0700

    fix(core/designsystem): fix dependencies

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -29,14 +29,18 @@ android {
 }
 
 dependencies {
-
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.runtime)
     api(libs.androidx.ui.text.google.fonts)
     api(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
 
     testImplementation(libs.junit)
+    testImplementation(composeBom)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
