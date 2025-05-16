08c6061f5d37bff3f39beb6d6e642a343522dcf3
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-16 12:23:39 +0700

    feat(core/ui): create cover component for track list

---
+++ core/ui/src/main/java/com/music/dzr/core/ui/TrackListCover.kt
@@ -0,0 +1,30 @@
+package com.music.dzr.core.ui
+
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import coil3.compose.AsyncImage
+import com.music.dzr.core.designsystem.R as designSystemR
+
+@Composable
+fun TrackListCover(
+    coverUrl: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier,
+    placeholder: Painter = painterResource(designSystemR.drawable.ic_placeholder_default)
+) {
+    AsyncImage(
+        model = coverUrl,
+        contentDescription = contentDescription,
+        placeholder = placeholder,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(ShapeDefaults.Small)
+    )
+}
