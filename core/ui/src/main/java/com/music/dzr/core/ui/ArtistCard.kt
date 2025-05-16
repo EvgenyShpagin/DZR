9921c041c821f95b8ec4bccea9f136eaefe42473
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-16 15:11:59 +0700

    feat(core/ui): add Artist card

---
+++ core/ui/src/main/java/com/music/dzr/core/ui/ArtistCard.kt
@@ -0,0 +1,80 @@
+package com.music.dzr.core.ui
+
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.music.dzr.core.designsystem.theme.DzrTheme
+import com.music.dzr.core.designsystem.R as designSystemR
+
+@Composable
+fun ArtistCard(
+    name: String,
+    pictureUrl: String,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .clip(ShapeDefaults.Small)
+            .combinedClickable(
+                role = Role.Button,
+                onClick = onClick,
+                onLongClick = onLongClick,
+                onLongClickLabel = stringResource(R.string.cd_show_more)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        InspectableAsyncImage(
+            model = pictureUrl,
+            contentDescription = stringResource(R.string.cd_artist),
+            placeholder = painterResource(designSystemR.drawable.ic_placeholder_default),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(128.dp)
+                .aspectRatio(1f)
+                .clip(CircleShape)
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ArtistCardPreview() {
+    DzrTheme {
+        ArtistCard(
+            name = "The Rolling Stones",
+            pictureUrl = "",
+            onClick = {},
+            onLongClick = {}
+        )
+    }
+}
