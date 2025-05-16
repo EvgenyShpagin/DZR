a2fc0f1754eb4fc9037b4fb23552a5fc6717cbf2
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-16 15:13:42 +0700

    refactor(core/ui): use InpectableAsyncImage in radio card

--- core/ui/src/main/java/com/music/dzr/core/ui/RadioCard.kt
+++ core/ui/src/main/java/com/music/dzr/core/ui/RadioCard.kt
@@ -4,23 +4,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.music.dzr.core.designsystem.theme.DzrTheme
 import com.music.dzr.core.designsystem.R as designSystemR
 
@@ -38,16 +37,18 @@ fun RadioCard(
             .clickable(role = Role.Button, onClick = onClick),
         contentAlignment = Alignment.Center
     ) {
-        AsyncImage(
+        InspectableAsyncImage(
             model = pictureUrl,
             contentDescription = stringResource(R.string.cd_radio),
-            placeholder = painterResource(designSystemR.drawable.ic_placeholder_default)
+            placeholder = painterResource(designSystemR.drawable.ic_placeholder_default),
+            contentScale = ContentScale.Crop
         )
         Text(
             name,
             style = MaterialTheme.typography.labelLarge,
             color = Color.White,
             textAlign = TextAlign.Center,
+            fontFamily = FontFamily.Monospace,
             modifier = Modifier.padding(16.dp)
         )
     }
@@ -57,15 +58,10 @@ fun RadioCard(
 @Composable
 private fun RadioCardPreview() {
     DzrTheme {
-        Surface(
-            color = MaterialTheme.colorScheme.primaryContainer,
-            modifier = Modifier.size(128.dp)
-        ) {
-            RadioCard(
-                name = "Summer Afternoon",
-                pictureUrl = "",
-                onClick = {}
-            )
-        }
+        RadioCard(
+            name = "Summer Afternoon",
+            pictureUrl = "",
+            onClick = {}
+        )
     }
 }
