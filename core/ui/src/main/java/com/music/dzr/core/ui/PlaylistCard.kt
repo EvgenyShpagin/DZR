d289e09d5dbe2e5939c57c2dd4ef930a00d67352
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-16 12:24:55 +0700

    feat(core/ui): use TrackListCover

--- core/ui/src/main/java/com/music/dzr/core/ui/PlaylistCard.kt
+++ core/ui/src/main/java/com/music/dzr/core/ui/PlaylistCard.kt
@@ -3,27 +3,20 @@ package com.music.dzr.core.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.music.dzr.core.designsystem.theme.DzrTheme
-import com.music.dzr.core.designsystem.R as designSystemR
 
 @Composable
 fun PlaylistCard(
@@ -36,14 +29,9 @@ fun PlaylistCard(
         modifier = modifier.clickable(role = Role.Button, onClick = onClick),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            model = pictureUrl,
-            contentDescription = stringResource(R.string.cd_playlist),
-            placeholder = painterResource(designSystemR.drawable.ic_placeholder_default),
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .aspectRatio(1f)
-                .clip(ShapeDefaults.Small)
+        TrackListCover(
+            coverUrl = pictureUrl,
+            contentDescription = stringResource(R.string.cd_playlist)
         )
         Spacer(Modifier.height(4.dp))
         Text(
