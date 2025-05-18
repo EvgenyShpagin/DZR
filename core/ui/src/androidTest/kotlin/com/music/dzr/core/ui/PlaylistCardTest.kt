3d8cff4ce0d3468d8637899e15aa22e2b2a15a19
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-18 18:34:50 +0700

    test(core/ui): add PlaylistCard test

---
+++ core/ui/src/androidTest/kotlin/com/music/dzr/core/ui/PlaylistCardTest.kt
@@ -0,0 +1,56 @@
+package com.music.dzr.core.ui
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PlaylistCardTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val mockOnClick = mockk<() -> Unit>()
+    private val name = "2000s Metal"
+    private val url = "https://example.com/playlist.jpg"
+
+    @Before
+    fun setUp() {
+        every { mockOnClick() } just runs
+
+        composeTestRule.setContent {
+            PlaylistCard(
+                name = name,
+                pictureUrl = url,
+                onClick = mockOnClick,
+                modifier = Modifier.testTag("playlistCard")
+            )
+        }
+    }
+
+    @Test
+    fun playlistCard_displaysCorrectName() {
+        composeTestRule
+            .onNodeWithText(name)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun playlistCard_invokesCallback_onClick() {
+        composeTestRule
+            .onNodeWithTag("playlistCard")
+            .performClick()
+        verify { mockOnClick() }
+    }
+}
