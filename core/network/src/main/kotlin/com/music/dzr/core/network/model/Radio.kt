ff5c71df2e260d5082dadd7f43b07bf7ab3e7789
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-20 19:16:47 +0700

    feat(core/network): add Radio model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Radio.kt
@@ -0,0 +1,19 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Radio(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val share: String,
+    val picture: String,
+    @SerialName("picture_small") val pictureSmall: String,
+    @SerialName("picture_medium") val pictureMedium: String,
+    @SerialName("picture_big") val pictureBig: String,
+    @SerialName("picture_xl") val pictureXl: String,
+    @SerialName("md5_image") val md5Image: String,
+    val tracklist: String
+)
