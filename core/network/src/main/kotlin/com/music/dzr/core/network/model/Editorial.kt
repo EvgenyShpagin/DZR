9b7f3fb81d1b1457a4571497aeee48b7cbaa4a94
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-20 19:18:01 +0700

    feat(core/network): add Editorial model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Editorial.kt
@@ -0,0 +1,15 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Editorial(
+    val id: Int,
+    val name: String,
+    val picture: String,
+    @SerialName("picture_small") val pictureSmall: String,
+    @SerialName("picture_medium") val pictureMedium: String,
+    @SerialName("picture_big") val pictureBig: String,
+    @SerialName("picture_xl") val pictureXl: String
+)
