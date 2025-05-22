b04c5e2cc5e66d938be8923bfb6b983670c9b55a
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-22 19:44:02 +0700

    feat(core/network): add all Radio models

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/Radio.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Radio.kt
@@ -15,5 +15,91 @@ data class Radio(
     @SerialName("picture_big") val pictureBig: String,
     @SerialName("picture_xl") val pictureXl: String,
     @SerialName("md5_image") val md5Image: String,
-    val tracklist: String
+    val tracklist: String,
+    val type: String
+)
+
+@Serializable
+data class RadioBrief(
+    val id: Int,
+    val title: String,
+    val picture: String,
+    @SerialName("picture_small") val pictureSmall: String,
+    @SerialName("picture_medium") val pictureMedium: String,
+    @SerialName("picture_big") val pictureBig: String,
+    @SerialName("picture_xl") val pictureXl: String,
+    val tracklist: String,
+    @SerialName("md5_image") val md5Image: String,
+    val type: String
+)
+
+@Serializable
+data class RadioByGenreList(
+    val data: List<RadioByGenre>
+)
+
+@Serializable
+data class RadioByGenre(
+    val id: Int,
+    val title: String,
+    val radios: List<RadioBrief>
+)
+
+@Serializable
+data class RadioTopTracks(
+    val data: List<RadioTrack>
+)
+
+@Serializable
+data class RadioTrack(
+    val id: Int,
+    val readable: Boolean,
+    val title: String,
+    @SerialName("title_short") val titleShort: String,
+    @SerialName("title_version") val titleVersion: String,
+    val link: String,
+    val duration: Int,
+    val rank: Int,
+    @SerialName("release_date") val releaseDate: String,
+    @SerialName("explicit_lyrics") val explicitLyrics: Boolean,
+    @SerialName("explicit_content_lyrics") val explicitContentLyrics: Int,
+    @SerialName("explicit_content_cover") val explicitContentCover: Int,
+    val preview: String,
+    val alternative: RadioTrackAlternative?,
+    @SerialName("md5_image") val md5Image: String,
+    val artist: ArtistBriefWithPic,
+    val album: RadioTrackAlbum
+)
+
+@Serializable
+data class RadioTrackAlternative(
+    val id: Int,
+    val readable: Boolean,
+    val title: String,
+    @SerialName("title_short") val titleShort: String,
+    @SerialName("title_version") val titleVersion: String,
+    val link: String,
+    val duration: Int,
+    val rank: Int,
+    @SerialName("explicit_lyrics") val explicitLyrics: Boolean,
+    @SerialName("explicit_content_lyrics") val explicitContentLyrics: Int,
+    @SerialName("explicit_content_cover") val explicitContentCover: Int,
+    val preview: String,
+    @SerialName("md5_image") val md5Image: String,
+    val artist: ArtistBriefWithPic,
+    val album: RadioTrackAlbum
+)
+
+@Serializable
+data class RadioTrackAlbum(
+    val id: Int,
+    val title: String,
+    val cover: String,
+    @SerialName("picture_small") val pictureSmall: String,
+    @SerialName("picture_medium") val pictureMedium: String,
+    @SerialName("picture_big") val pictureBig: String,
+    @SerialName("picture_xl") val pictureXl: String,
+    @SerialName("md5_image") val md5Image: String,
+    val tracklist: String,
+    val type: String
 )
