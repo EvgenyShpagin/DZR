4acfcc91bef6c67b5f2ca705d78b975e072df8ea
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-22 19:42:31 +0700

    feat(core/network): add all Track models

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Track.kt
@@ -0,0 +1,50 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Track(
+    val id: Int,
+    val readable: Boolean,
+    val title: String,
+    @SerialName("title_short") val titleShort: String,
+    @SerialName("title_version") val titleVersion: String,
+    val isrc: String,
+    val link: String,
+    val share: String,
+    val duration: Int,
+    @SerialName("track_position") val trackPosition: Int,
+    @SerialName("disk_number") val diskNumber: Int,
+    val rank: Int,
+    @SerialName("release_date") val releaseDate: String,
+    @SerialName("explicit_lyrics") val explicitLyrics: Boolean,
+    @SerialName("explicit_content_lyrics") val explicitContentLyrics: Int,
+    @SerialName("explicit_content_cover") val explicitContentCover: Int,
+    val preview: String,
+    val bpm: Float,
+    val gain: Float,
+    @SerialName("available_countries") val availableCountries: List<String>,
+    val alternative: Track? = null,
+    val contributors: List<Contributor>,
+    @SerialName("md5_image") val md5Image: String,
+    @SerialName("track_token") val trackToken: String,
+    val artist: Artist,
+    val album: TrackAlbum,
+    val type: String
+)
+
+@Serializable
+data class TrackAlbum(
+    val id: Int,
+    val title: String,
+    val link: String,
+    val cover: String,
+    @SerialName("cover_small") val coverSmall: String,
+    @SerialName("cover_medium") val coverMedium: String,
+    @SerialName("cover_big") val coverBig: String,
+    @SerialName("cover_xl") val coverXl: String,
+    @SerialName("release_date") val releaseDate: String,
+    val tracklist: String,
+    val type: String
+)
