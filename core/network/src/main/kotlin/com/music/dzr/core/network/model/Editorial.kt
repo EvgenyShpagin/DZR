29db33b109df9ad05eedf034a7cc5b8e64652171
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-22 19:41:29 +0700

    feat(core/network): add all Editorial models

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/Editorial.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Editorial.kt
@@ -1,7 +1,13 @@
 package com.music.dzr.core.network.model
 
+import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+@Serializable
+data class Editorials(
+    val data: List<Editorial>
+)
 
 @Serializable
 data class Editorial(
@@ -11,5 +17,41 @@ data class Editorial(
     @SerialName("picture_small") val pictureSmall: String,
     @SerialName("picture_medium") val pictureMedium: String,
     @SerialName("picture_big") val pictureBig: String,
-    @SerialName("picture_xl") val pictureXl: String
+    @SerialName("picture_xl") val pictureXl: String,
+    val type: String
+)
+
+@Serializable
+data class EditorialReleasesAlbum(
+    val id: Int,
+    val title: String,
+    val cover: String,
+    @SerialName("cover_small") val coverSmall: String,
+    @SerialName("cover_medium") val coverMedium: String,
+    @SerialName("cover_big") val coverBig: String,
+    @SerialName("cover_xl") val coverXl: String,
+    @SerialName("md5_image") val md5Image: String,
+    @SerialName("release_date") val releaseDate: Instant,
+    val tracklist: String,
+    val artist: ArtistBrief,
+    val type: String
+)
+
+@Serializable
+data class EditorialSelectionAlbum(
+    val id: Int,
+    val title: String,
+    val cover: String,
+    @SerialName("cover_small") val coverSmall: String,
+    @SerialName("cover_medium") val coverMedium: String,
+    @SerialName("cover_big") val coverBig: String,
+    @SerialName("cover_xl") val coverXl: String,
+    @SerialName("md5_image") val md5Image: String,
+    @SerialName("genre_id") val genreId: Int,
+    @SerialName("record_type") val recordType: String,
+    val tracklist: String,
+    @SerialName("explicit_lyrics") val explicitLyrics: Boolean,
+    val artist: ArtistBrief,
+    val type: String
+
 )
