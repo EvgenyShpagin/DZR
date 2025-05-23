cba32951e4c875a178a1162e4b43137566dbc522
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-05-23 15:26:14 +0700

    docs(core/model): add PlaylistCreator model docs

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaylistCreator.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaylistCreator.kt
@@ -3,6 +3,9 @@ package com.music.dzr.core.network.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents the creator of a playlist.
+ */
 @Serializable
 data class PlaylistCreator(
     @SerialName("id") val userId: Int,
