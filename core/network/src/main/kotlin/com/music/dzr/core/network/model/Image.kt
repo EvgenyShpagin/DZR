67076a63796e1e2c36e45d52e3130c0264c87d2a
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-08 14:23:13 +0700

    chore(core/network): correct Image docs

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/Image.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Image.kt
@@ -3,7 +3,7 @@ package com.music.dzr.core.network.model
 import kotlinx.serialization.Serializable
 
 /**
- * Represents an image object for album artwork.
+ * Represents an image object for various Spotify objects.
  */
 @Serializable
 data class Image(
