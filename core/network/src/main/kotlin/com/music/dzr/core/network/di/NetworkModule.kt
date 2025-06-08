8b452065151e81cd486b136d8e34ed083c15b9b4
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-08 11:45:49 +0700

    refactor(core/network): delete all Deezer API

--- core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
@@ -1,15 +1,5 @@
 package com.music.dzr.core.network.di
 
-import com.music.dzr.core.network.api.AlbumApi
-import com.music.dzr.core.network.api.ArtistApi
-import com.music.dzr.core.network.api.ChartApi
-import com.music.dzr.core.network.api.EditorialApi
-import com.music.dzr.core.network.api.GenreApi
-import com.music.dzr.core.network.api.PlaylistApi
-import com.music.dzr.core.network.api.RadioApi
-import com.music.dzr.core.network.api.SearchApi
-import com.music.dzr.core.network.api.TrackApi
-import com.music.dzr.core.network.api.UserApi
 import com.music.dzr.core.network.retrofit.NetworkErrorResponseParser
 import com.music.dzr.core.network.retrofit.NetworkResponseCallAdapterFactory
 import kotlinx.serialization.json.Json
@@ -39,25 +29,5 @@ val networkModule = module {
             .addCallAdapterFactory(NetworkResponseCallAdapterFactory(get()))
             .build()
     }
-
-    single<AlbumApi> { get<Retrofit>().create(AlbumApi::class.java) }
-
-    single<ArtistApi> { get<Retrofit>().create(ArtistApi::class.java) }
-
-    single<ChartApi> { get<Retrofit>().create(ChartApi::class.java) }
-
-    single<EditorialApi> { get<Retrofit>().create(EditorialApi::class.java) }
-
-    single<GenreApi> { get<Retrofit>().create(GenreApi::class.java) }
-
-    single<PlaylistApi> { get<Retrofit>().create(PlaylistApi::class.java) }
-
-    single<RadioApi> { get<Retrofit>().create(RadioApi::class.java) }
-
-    single<SearchApi> { get<Retrofit>().create(SearchApi::class.java) }
-
-    single<TrackApi> { get<Retrofit>().create(TrackApi::class.java) }
-
-    single<UserApi> { get<Retrofit>().create(UserApi::class.java) }
 
 }
