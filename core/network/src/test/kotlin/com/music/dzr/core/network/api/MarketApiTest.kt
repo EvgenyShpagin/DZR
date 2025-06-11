419e2791400d718e6d9da02d13700bdcde4b9a8f
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 14:19:21 +0700

    test(core/network): add MarketApi tests

---
+++ core/network/src/test/kotlin/com/music/dzr/core/network/api/MarketApiTest.kt
@@ -0,0 +1,60 @@
+package com.music.dzr.core.network.api
+
+import kotlinx.coroutines.test.runTest
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Test
+
+class MarketApiTest {
+
+    private lateinit var server: MockWebServer
+    private lateinit var api: MarketApi
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        server.start()
+        api = createApi(server.url("/"))
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    @Test
+    fun getMarkets_returnsData_whenServerRespondsWith200() = runTest {
+        // Arrange: enqueue a 200 response with a sample album JSON
+        server.enqueueResponseFromAssets("available_markets_response.json")
+
+        // Act: call the API
+        val response = api.getAvailableMarkets()
+
+        // Assert: correct data is fetched
+        assertNull(response.error)
+        assertNotNull(response.data)
+        with(response.data!!) {
+            assertEquals(185, markets.count())
+            assertEquals("AD", markets.first())
+            assertEquals("ZW", markets.last())
+        }
+    }
+
+    @Test
+    fun getMarkets_usesCorrectPathAndMethod_onRequestWithMarket() = runTest {
+        // Arrange: enqueue a 200 response with a sample album JSON
+        server.enqueueResponseFromAssets("available_markets_response.json")
+
+        // Act: call the API
+        api.getAvailableMarkets()
+
+        // Assert: request path and method
+        val recordedRequest = server.takeRequest()
+        assertEquals("/markets", recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+}
