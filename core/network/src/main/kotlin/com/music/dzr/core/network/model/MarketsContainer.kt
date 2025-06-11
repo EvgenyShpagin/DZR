012fb76552afafb98cef9c473b0b791d5e456b14
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 14:17:15 +0700

    feat(core/network): add Market models

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/MarketsContainer.kt
@@ -0,0 +1,11 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Response wrapper for getting multiple Markets
+ * @property markets A markets object with an array of country codes
+ * Example: `["CA","BR","IT"]`
+ */
+@Serializable
+data class MarketsContainer(val markets: List<String>)
