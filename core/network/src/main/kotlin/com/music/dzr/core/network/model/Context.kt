49e3af953a8b29d27ac54e50bc298d5980cbdf3e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 19:13:02 +0700

    feat(core/network): add playback Context model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Context.kt
@@ -0,0 +1,21 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents the context of the playback, such as a playlist or album.
+ *
+ * @property type The type of the context, e.g., "album", "playlist".
+ * @property href A link to the Web API endpoint providing full details of the context.
+ * @property externalUrls External URLs for this context.
+ * @property uri The Spotify URI for the context.
+ */
+@Serializable
+data class Context(
+    val type: String,
+    val href: String,
+    @SerialName("external_urls")
+    val externalUrls: ExternalUrls,
+    val uri: String
+) 
