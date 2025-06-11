5829b3625352161420d9476cdd48a00071ff879e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 18:48:52 +0700

    feat(core/network): add Device models

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/DevicesContainer.kt
@@ -0,0 +1,13 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * A container for a list of [Device] objects.
+ *
+ * @property devices A list of devices.
+ */
+@Serializable
+data class DevicesContainer(
+    val devices: List<Device>
+) 
