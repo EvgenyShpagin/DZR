9d26d1ab2385ae4d48731ad283df9cf458317ccb
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 18:36:51 +0700

    feat(core/network): add track queue model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Queue.kt
@@ -0,0 +1,17 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents the user's playback queue.
+ *
+ * @property currentlyPlaying The currently playing track or episode. Can be `null` if nothing is playing.
+ * @property queue The tracks or episodes in the queue.
+ */
+@Serializable
+data class Queue(
+    @SerialName("currently_playing")
+    val currentlyPlaying: Track?,
+    val queue: List<Track>
+) 
