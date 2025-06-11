faed096535d35c9770b7ca17a3df3f80d5500e89
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-11 19:15:43 +0700

    feat(core/network): add PlayHistory model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/PlayHistory.kt
@@ -0,0 +1,20 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents an entry in the user's recently played tracks.
+ *
+ * @property track The track the user listened to.
+ * @property playedAt The date and time the track was played.
+ * @property context The context the track was played from.
+ */
+@Serializable
+data class PlayHistory(
+    val track: Track,
+    @SerialName("played_at")
+    val playedAt: Instant,
+    val context: Context?
+) 
