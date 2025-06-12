55dbb9f47a5107697d13f25a00ae16bb44185c90
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-12 14:21:54 +0700

    chore(core/network): update PlaybackOptions doc

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaybackOptions.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaybackOptions.kt
@@ -5,13 +5,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * A data class representing options for starting or resuming playback.
+ * Options for starting or resuming playback.
  * Used as the request body for the [PlayerApi.startOrResumePlayback] endpoint.
  *
- * @property contextUri The URI of the context to play, e.g., an album, artist, or playlist.
- * @property uris A list of track URIs to play.
- * @property offset Indicates where in the context playback should start.
- * @property positionMs The position in milliseconds from which to start playback.
+ * @property contextUri The URI of the context to play.
+ * @property uris A list of Spotify track URIs to play.
+ * @property offset Indicates from where in the context playback should start.
+ * @property positionMs The position in milliseconds to start playback from.
  */
 @Serializable
 data class PlaybackOptions(
