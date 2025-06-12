401a293dfe6533478c289b0674ad9a8bd51a0235
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-12 11:25:38 +0700

    chore(core/network): add docs link

--- core/network/src/main/kotlin/com/music/dzr/core/network/api/PlayerApi.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/api/PlayerApi.kt
@@ -17,6 +17,7 @@ import retrofit2.http.Query
 
 /**
  * A service for interacting with the Spotify Player API.
+ * @see <a href="https://developer.spotify.com/documentation/web-api/">Spotify API</a>
  */
 interface PlayerApi {
 
