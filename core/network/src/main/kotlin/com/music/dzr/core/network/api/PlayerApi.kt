18a79dd0cea8733d806efc6143291c4ebcbb3a83
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-12 11:08:20 +0700

    feat(core/network): add transferPlayback API

--- core/network/src/main/kotlin/com/music/dzr/core/network/api/PlayerApi.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/api/PlayerApi.kt
@@ -3,7 +3,9 @@ package com.music.dzr.core.network.api
 import com.music.dzr.core.network.model.NetworkResponse
 import com.music.dzr.core.network.model.PermissionScope
 import com.music.dzr.core.network.model.PlaybackState
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 /**
@@ -22,5 +24,20 @@ interface PlayerApi {
     suspend fun getPlaybackState(
         @Query("market") market: String? = null,
     ): NetworkResponse<PlaybackState>
+
+    /**
+     * Transfer playback to a new device and determine if it should start playing.
+     *
+     * Requires [PermissionScope.UserModifyPlaybackState]
+     *
+     * @param deviceIds A JSON object containing a single key "device_ids" which is an array of device IDs to transfer playback to.
+     * **Only a single `device_id` is currently supported**
+     * @param play `true`: ensure playback happens on new device. `false`: keep the current playback state.
+     */
+    @PUT("me/player")
+    suspend fun transferPlayback(
+        @Body deviceIds: Map<String, List<String>>,
+        @Query("play") play: Boolean = false
+    ): NetworkResponse<Unit>
 
 } 
