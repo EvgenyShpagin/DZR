11898b6b655133804b606d5f36a287d755a8faab
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-16 17:50:55 +0700

    test(core/network): add TrackApi tests

---
+++ core/network/src/test/kotlin/com/music/dzr/core/network/api/TrackApiTest.kt
@@ -0,0 +1,236 @@
+package com.music.dzr.core.network.api
+
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Instant
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Test
+
+class TrackApiTest {
+
+    private lateinit var server: MockWebServer
+    private lateinit var api: TrackApi
+
+    private val id = "1"
+    private val ids = listOf("1", "2", "3")
+    private val commaSeparatedIds = "1,2,3"
+    private val encodedCommaSeparatedIds = "1%2C2%2C3"
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        server.start()
+        api = createApi(server.url("/"))
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    @Test
+    fun getTrack_returnsData_whenServerRespondsWith200() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/track.json")
+        // Act
+        val response = api.getTrack(id)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+        with(response.data!!) {
+            assertEquals("11dFghVXANMlKmJXsNCbNl", id)
+            assertEquals("Cut To The Feeling", name)
+        }
+    }
+
+    @Test
+    fun getTrack_usesCorrectPathAndMethod_onRequestWithMarket() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/track.json")
+        // Act
+        api.getTrack(id, market = "US")
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/tracks/$id?market=US", recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+
+    @Test
+    fun getMultipleTracks_returnsData_whenServerRespondsWith200() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/multiple-tracks.json")
+        // Act
+        val response = api.getMultipleTracks(commaSeparatedIds)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+        with(response.data!!) {
+            assertEquals(3, tracks.count())
+            assertEquals("Knights of Cydonia", tracks.first().name)
+        }
+    }
+
+    @Test
+    fun getMultipleTracks_usesCorrectPathAndMethod_onRequestWithMarket() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/multiple-tracks.json")
+        // Act
+        api.getMultipleTracks(commaSeparatedIds, market = "GB")
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/tracks?ids=$encodedCommaSeparatedIds&market=GB", recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+
+    @Test
+    fun getUsersSavedTracks_returnsData_on200CodeResponse() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/user-saved-tracks.json")
+        // Act
+        val response = api.getUsersSavedTracks()
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+        with(response.data!!) {
+            assertEquals(3, items.count())
+            with(items.first()) {
+                assertEquals(Instant.parse("2025-06-14T13:45:57Z"), addedAt)
+                assertEquals("1BKT2I9x4RGKaKqW4up34s", track.id)
+            }
+        }
+    }
+
+    @Test
+    fun getUsersSavedTracks_usesCorrectPathAndMethod_onResponseWithParams() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/user-saved-tracks.json")
+        // Act
+        api.getUsersSavedTracks(limit = 20, offset = 0, market = "FR")
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks?limit=20&offset=0&market=FR", recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+
+    @Test
+    fun saveTracksForUser_returnsData_on200CodeResponseWithQuery() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        val response = api.saveTracksForUser(commaSeparatedIds)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+    }
+
+    @Test
+    fun saveTracksForUser_usesCorrectPathAndMethod_onRequestWithQuery() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        api.saveTracksForUser(commaSeparatedIds)
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks?ids=$encodedCommaSeparatedIds", recordedRequest.path)
+        assertEquals("PUT", recordedRequest.method)
+    }
+
+    @Test
+    fun saveTracksForUser_returnsData_on200CodeResponseWithBody() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        val response = api.saveTracksForUser(ids)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+    }
+
+    @Test
+    fun saveTracksForUser_usesCorrectBodyAndMethod_onRequestWithBody() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        api.saveTracksForUser(ids)
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks", recordedRequest.path)
+        assertEquals("PUT", recordedRequest.method)
+        assertEquals(ids.toJsonArray(), recordedRequest.body.readUtf8())
+    }
+
+    @Test
+    fun removeTracksForUser_returnsData_on200CodeResponseWithQuery() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        val response = api.removeTracksForUser(commaSeparatedIds)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+    }
+
+    @Test
+    fun removeTracksForUser_usesCorrectPathAndMethod_onRequestWithQuery() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        api.removeTracksForUser(commaSeparatedIds)
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks?ids=$encodedCommaSeparatedIds", recordedRequest.path)
+        assertEquals("DELETE", recordedRequest.method)
+    }
+
+    @Test
+    fun removeTracksForUser_returnsData_on200CodeResponseWithBody() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        val response = api.removeTracksForUser(ids)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+    }
+
+    @Test
+    fun removeTracksForUser_usesCorrectBodyAndMethod_onRequestWithBody() = runTest {
+        // Arrange
+        server.enqueueEmptyResponse()
+        // Act
+        api.removeTracksForUser(ids)
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks", recordedRequest.path)
+        assertEquals("DELETE", recordedRequest.method)
+        assertEquals(ids.toJsonArray(), recordedRequest.body.readUtf8())
+    }
+
+    @Test
+    fun checkUsersSavedTracks_returnsData_on200CodeResponse() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/check-user-saved-tracks.json")
+        // Act
+        val response = api.checkUsersSavedTracks(commaSeparatedIds)
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+        assertEquals(listOf(false, true), response.data!!)
+    }
+
+    @Test
+    fun checkUsersSavedTracks_usesCorrectPathAndMethod_onRequest() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/track/check-user-saved-tracks.json")
+        // Act
+        api.checkUsersSavedTracks(commaSeparatedIds)
+        // Assert
+        val recordedRequest = server.takeRequest()
+        assertEquals("/me/tracks/contains?ids=$encodedCommaSeparatedIds", recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+} 
