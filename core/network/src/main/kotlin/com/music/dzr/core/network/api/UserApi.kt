5bd50610daf3a25468e5dd494dbdbe17569d415c
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-16 18:50:10 +0700

    feat(core/network): add getUserTopItems api methods

--- core/network/src/main/kotlin/com/music/dzr/core/network/api/UserApi.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/api/UserApi.kt
@@ -1,9 +1,13 @@
 package com.music.dzr.core.network.api
 
+import com.music.dzr.core.network.model.Artist
 import com.music.dzr.core.network.model.CurrentUser
 import com.music.dzr.core.network.model.NetworkResponse
+import com.music.dzr.core.network.model.PaginatedList
 import com.music.dzr.core.network.model.PermissionScope
+import com.music.dzr.core.network.model.Track
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * A service for interacting with the Spotify User API.
@@ -21,5 +25,41 @@ interface UserApi {
      */
     @GET("me")
     suspend fun getCurrentUserProfile(): NetworkResponse<CurrentUser>
+
+    /**
+     * Get the current user's top artists based on calculated affinity.
+     *
+     * Requires [PermissionScope.UserTopRead].
+     *
+     * @see <a href="https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks">Get User's Top Items</a>
+     *
+     * @param timeRange Over what time frame the affinities are computed. Default: medium_term.
+     * @param limit The number of entities to return. Default: 20. Minimum: 1. Maximum: 50.
+     * @param offset The index of the first entity to return.
+     */
+    @GET("me/top/artists")
+    suspend fun getUsersTopArtists(
+        @Query("time_range") timeRange: String? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("offset") offset: Int? = null,
+    ): NetworkResponse<PaginatedList<Artist>>
+
+    /**
+     * Get the current user's top tracks based on calculated affinity.
+     *
+     * Requires [PermissionScope.UserTopRead].
+     *
+     * @see <a href="https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks">Get User's Top Items</a>
+     *
+     * @param timeRange Over what time frame the affinities are computed. Default: medium_term.
+     * @param limit The number of entities to return. Default: 20. Minimum: 1. Maximum: 50.
+     * @param offset The index of the first entity to return.
+     */
+    @GET("me/top/tracks")
+    suspend fun getUsersTopTracks(
+        @Query("time_range") timeRange: String? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("offset") offset: Int? = null,
+    ): NetworkResponse<PaginatedList<Track>>
 
 }
