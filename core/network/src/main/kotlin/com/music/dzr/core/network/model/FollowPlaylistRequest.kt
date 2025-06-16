28878964e71c9f3c06026bac85ffc1e7b4de5085
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-16 18:45:31 +0700

    refactor(core/network): move FollowPlaylistRequest model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/FollowPlaylistRequest.kt
@@ -0,0 +1,13 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Request body for following a playlist.
+ *
+ * @property public If `true` the playlist will be included in user's public playlists, if `false` if false it will remain private.
+ */
+@Serializable
+data class FollowPlaylistRequest(
+    val public: Boolean = true,
+)
