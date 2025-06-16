b4b5b0885bd1646b1cbbbf71bf5b400d7603380a
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-16 18:44:23 +0700

    refactor(core/network): move ExplicitContent to CurrentUser

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/CurrentUser.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/CurrentUser.kt
@@ -41,6 +41,20 @@ data class CurrentUser(
 )
 
 /**
+ * The user's explicit content settings.
+ *
+ * @property filterEnabled When true, indicates that explicit content should not be played.
+ * @property filterLocked When true, indicates that the explicit content setting is locked and can't be changed by the user.
+ */
+@Serializable
+data class ExplicitContent(
+    @SerialName("filter_enabled")
+    val filterEnabled: Boolean,
+    @SerialName("filter_locked")
+    val filterLocked: Boolean
+)
+
+/**
  * Represents the user's Spotify subscription level.
  *
  * The subscription level [Open] can be considered the same as [Free]
