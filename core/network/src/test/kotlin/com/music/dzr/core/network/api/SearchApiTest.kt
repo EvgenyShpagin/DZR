2f03e6d5cb9c83ebfe56b9a91df2f0114477b08f
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-16 13:20:41 +0700

    test(core/network): add SearchApi tests

---
+++ core/network/src/test/kotlin/com/music/dzr/core/network/api/SearchApiTest.kt
@@ -0,0 +1,85 @@
+package com.music.dzr.core.network.api
+
+import kotlinx.coroutines.test.runTest
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Test
+
+class SearchApiTest {
+
+    private lateinit var server: MockWebServer
+    private lateinit var api: SearchApi
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        server.start()
+        api = createApi(server.url("/"))
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    @Test
+    fun search_returnsData_whenServerRespondsWith200() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/search/search-results.json")
+
+        // Act
+        val response = api.search(query = "dummy", type = "dummy")
+
+        // Assert
+        assertNull(response.error)
+        assertNotNull(response.data)
+        with(response.data!!) {
+            assertNotNull(tracks)
+            assertEquals(20, tracks!!.limit)
+            assertEquals(829, tracks!!.total)
+            with(tracks!!.items.first()) {
+                assertEquals("superman (eminem, dina rae) [sped up version]", album.name)
+                assertEquals("superman (eminem, dina rae) - sped up version", name)
+            }
+        }
+    }
+
+    @Test
+    fun search_usesCorrectPathAndMethod_onRequestWithAllParameters() = runTest {
+        // Arrange
+        server.enqueueResponseFromAssets("responses/search/search-results.json")
+        val spaceEncodedQuery = "remaster%20track:Doxy%20artist:Miles%20Davis"
+        val encodedQuery = "remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis"
+        val type = "album,track"
+        val encodedType = "album%2Ctrack"
+        val market = "US"
+        val limit = 10
+        val offset = 5
+        val includeExternal = "audio"
+
+        // Act
+        api.search(
+            query = spaceEncodedQuery,
+            type = type,
+            market = market,
+            limit = limit,
+            offset = offset,
+            includeExternal = includeExternal
+        )
+
+        // Assert
+        val recordedRequest = server.takeRequest()
+        val expectedPath = "/search?q=$encodedQuery" +
+                "&type=$encodedType" +
+                "&market=$market" +
+                "&limit=$limit" +
+                "&offset=$offset" +
+                "&include_external=$includeExternal"
+        assertEquals(expectedPath, recordedRequest.path)
+        assertEquals("GET", recordedRequest.method)
+    }
+} 
