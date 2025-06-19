f7bf5555b3a37824673a335e6a39145b3d5591a6
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-19 13:48:22 +0700

    feat(core/network): implement UrlParameter by TimeRange

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/TimeRange.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/TimeRange.kt
@@ -1,5 +1,7 @@
 package com.music.dzr.core.network.model
 
+import com.music.dzr.core.network.retrofit.UrlParameter
+import com.music.dzr.core.network.util.getSerializedName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -8,7 +10,7 @@ import kotlinx.serialization.Serializable
  * Used for getting top user tracks/artists.
  */
 @Serializable
-enum class TimeRange {
+enum class TimeRange : UrlParameter {
     /**
      * Calculated from ~1 year of data and including all new data as it becomes available.
      */
@@ -25,5 +27,7 @@ enum class TimeRange {
      * Approximately last 4 weeks.
      */
     @SerialName("short_term")
-    ShortTerm
+    ShortTerm;
+
+    override val urlValue get() = getSerializedName()
 }
