47702520f3203eccb40ced37bed0fb913cd5e79b
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-19 18:23:28 +0700

    test(core/network): add Offset serializer tests

---
+++ core/network/src/test/kotlin/com/music/dzr/core/network/util/OffsetSerializerTest.kt
@@ -0,0 +1,32 @@
+package com.music.dzr.core.network.util
+
+import com.music.dzr.core.network.model.Offset
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class OffsetSerializerTest {
+
+    @Test
+    fun serializesAndDeserializes_offsetByPosition() {
+        val offset = Offset.ByPosition(5)
+        val jsonString = Json.encodeToString(offset)
+        val decodedOffset = Json.decodeFromString<Offset>(jsonString)
+        assertEquals(offset, decodedOffset)
+    }
+
+    @Test
+    fun serializesAndDeserializes_OffsetByUri() {
+        val offset = Offset.ByUri("spotify:track:12345")
+        val jsonString = Json.encodeToString(offset)
+        val decodedOffset = Json.decodeFromString<Offset>(jsonString)
+        assertEquals(offset, decodedOffset)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun throwsException_onInvalidJson() {
+        val invalidJson = """{"invalid_key":"invalid_value"}"""
+        Json.decodeFromString<Offset>(invalidJson)
+    }
+}
