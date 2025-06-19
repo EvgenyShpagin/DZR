b7a44dbc0792a5e682b91327fc6f721dada83ba1
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-19 18:34:06 +0700

    refactor(core/network): improve OffsetSerializer impl

--- core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaybackOptions.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/PlaybackOptions.kt
@@ -34,11 +34,13 @@ sealed interface Offset {
      * Specifies the track to start playback from by its position in the context.
      * @property position The zero-based index of the track.
      */
+    @Serializable
     data class ByPosition(val position: Int) : Offset
 
     /**
      * Specifies the track to start playback from by its URI.
      * @property uri The URI of the track.
      */
+    @Serializable
     data class ByUri(val uri: String) : Offset
 }
