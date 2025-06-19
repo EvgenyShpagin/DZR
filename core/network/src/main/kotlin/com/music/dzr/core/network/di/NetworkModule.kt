e7cc5a8503b89a674d6c722ca688e10786110f06
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-19 20:17:56 +0700

    feat(core/network): add APIs to networkModule

--- core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
@@ -1,5 +1,14 @@
 package com.music.dzr.core.network.di
 
+import com.music.dzr.core.network.api.AlbumApi
+import com.music.dzr.core.network.api.ArtistApi
+import com.music.dzr.core.network.api.BrowseCategoryApi
+import com.music.dzr.core.network.api.MarketApi
+import com.music.dzr.core.network.api.PlayerApi
+import com.music.dzr.core.network.api.PlaylistApi
+import com.music.dzr.core.network.api.SearchApi
+import com.music.dzr.core.network.api.TrackApi
+import com.music.dzr.core.network.api.UserApi
 import com.music.dzr.core.network.retrofit.NetworkErrorResponseParser
 import com.music.dzr.core.network.retrofit.NetworkResponseCallAdapterFactory
 import com.music.dzr.core.network.retrofit.UrlParameterConverterFactory
@@ -8,6 +17,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
 
 private const val DZR_BASE_URL = "https://api.deezer.com/"
 
@@ -32,4 +42,13 @@ val networkModule = module {
             .build()
     }
 
+    single { get<Retrofit>().create<AlbumApi>() }
+    single { get<Retrofit>().create<ArtistApi>() }
+    single { get<Retrofit>().create<BrowseCategoryApi>() }
+    single { get<Retrofit>().create<MarketApi>() }
+    single { get<Retrofit>().create<PlayerApi>() }
+    single { get<Retrofit>().create<PlaylistApi>() }
+    single { get<Retrofit>().create<SearchApi>() }
+    single { get<Retrofit>().create<TrackApi>() }
+    single { get<Retrofit>().create<UserApi>() }
 }
