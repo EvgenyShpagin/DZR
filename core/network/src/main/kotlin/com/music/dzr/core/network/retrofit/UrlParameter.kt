652493ceb822d528dd1798491f280b469b722e85
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-19 12:59:55 +0700

    chore(core/network): refer to UrlParameterConverterFactory in doc

--- core/network/src/main/kotlin/com/music/dzr/core/network/retrofit/UrlParameter.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/retrofit/UrlParameter.kt
@@ -7,8 +7,7 @@ package com.music.dzr.core.network.retrofit
  * This provides a more explicit contract than overriding [toString] for providing
  * a value to be used in a URL.
  *
- * A custom [retrofit2.Converter.Factory] should be used to recognize this interface
- * and call [urlValue] to get the string representation.
+ * Used in [UrlParameterConverterFactory]
  */
 interface UrlParameter {
     /**
