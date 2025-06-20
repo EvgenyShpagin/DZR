544771b7aa3637081455cb47db92ea44ec73c0bc
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-20 11:30:35 +0700

    feat(core/network): add Token model

---
+++ core/network/src/main/kotlin/com/music/dzr/core/network/model/Token.kt
@@ -0,0 +1,26 @@
+package com.music.dzr.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents the response from the Spotify token endpoint.
+ *
+ * @property accessToken The access token to be used for API calls.
+ * @property refreshToken A token that can be used to obtain a new access token.
+ * @property expiresIn The time period in seconds for which the access token is valid.
+ * @property scope A space-separated list of scopes which have been granted.
+ * @property tokenType How the access token may be used, always "Bearer".
+ */
+@Serializable
+data class Token(
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("refresh_token")
+    val refreshToken: String,
+    @SerialName("expires_in")
+    val expiresIn: Int,
+    val scope: String,
+    @SerialName("token_type")
+    val tokenType: String
+) 
