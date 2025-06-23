78169e9fc75ccf8a385a56188ccb4ebe5ecc09e6
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-23 13:35:42 +0700

    feat(core/oauth): create Token model

---
+++ core/oauth/src/main/kotlin/com/music/dzr/core/oauth/model/Token.kt
@@ -0,0 +1,25 @@
+package com.music.dzr.core.oauth.model
+
+/**
+ * A pure domain model representing an OAuth 2.0 token grant, compliant with RFC 6749.
+ *
+ * [Successful token response description](https://datatracker.ietf.org/doc/html/rfc6749#section-5.1)
+ *
+ * This class is independent of any networking or serialization library. It describes the
+ * essential components of an access token grant from the application's business logic perspective.
+ *
+ * @property accessToken The access token issued by the authorization server.
+ * @property tokenType The type of the token, typically "Bearer". This determines how the token
+ *                     is formatted in the Authorization header.
+ * @property expiresInSeconds The lifetime of the access token in seconds.
+ * @property refreshToken The token that can be used to obtain a new access token. It can be null,
+ *                      as it's not always returned, especially on subsequent token refreshes.
+ * @property scope A space-separated list of scopes that have been granted for this token.
+ */
+data class Token(
+    val accessToken: String,
+    val tokenType: String,
+    val expiresInSeconds: Int,
+    val refreshToken: String?,
+    val scope: String?
+)
