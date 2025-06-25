8b600f6b1eee5fe104d5db3097e4f9b7ee1a89ed
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-25 15:05:22 +0700

    fix(core/network): update PlaylistApiTest imports

--- core/network/src/test/kotlin/com/music/dzr/core/network/api/PlaylistApiTest.kt
+++ core/network/src/test/kotlin/com/music/dzr/core/network/api/PlaylistApiTest.kt
@@ -1,11 +1,11 @@
 package com.music.dzr.core.network.api
 
-import com.music.dzr.core.network.model.playlist.request.AddTracksToPlaylistRequest
-import com.music.dzr.core.network.model.playlist.request.ChangePlaylistDetailsRequest
+import com.music.dzr.core.network.model.playlist.AddTracksToPlaylistRequest
+import com.music.dzr.core.network.model.playlist.ChangePlaylistDetailsRequest
 import com.music.dzr.core.network.model.playlist.CreatePlaylistRequest
-import com.music.dzr.core.network.model.playlist.request.RemovePlaylistTracksRequest
-import com.music.dzr.core.network.model.playlist.request.TrackToRemove
-import com.music.dzr.core.network.model.playlist.request.UpdatePlaylistItemsRequest
+import com.music.dzr.core.network.model.playlist.RemovePlaylistTracksRequest
+import com.music.dzr.core.network.model.playlist.TrackToRemove
+import com.music.dzr.core.network.model.playlist.UpdatePlaylistItemsRequest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.encodeToString
