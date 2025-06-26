4ff19f979dcf0cdfe93e9fb9b9c3f79c6027f674
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-26 12:35:06 +0700

    refactor(core/network): make networkModule public

--- core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
@@ -6,7 +6,7 @@ import org.koin.dsl.module
  * A Koin module that encapsulates all dependencies from the core:network.
  * This is the single entry point for other modules to include all network-related dependencies.
  */
-internal val networkModule = module {
+val networkModule = module {
     includes(
         httpClientModule,
         retrofitModule,
