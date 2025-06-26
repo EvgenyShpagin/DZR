53159154639038145cd16407b50cfd4680b25a2d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-26 13:48:15 +0700

    chore(core/network): use libs.koin.test

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockwebserver)
+    testImplementation(libs.koin.test)
 }
