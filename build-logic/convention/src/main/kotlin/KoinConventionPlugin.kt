50fc4f40542700aa7bccff2d8e55a1e356b67ca2
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-28 18:45:35 +0700

    chore(build-config/convention): add Koin convention plugin

---
+++ build-logic/convention/src/main/kotlin/KoinConventionPlugin.kt
@@ -0,0 +1,32 @@
+import com.android.build.gradle.api.AndroidBasePlugin
+import com.music.dzr.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class KoinConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            val bom = libs.findLibrary("koin.bom").get()
+            dependencies {
+                "implementation"(platform(bom))
+                "testImplementation"(platform(bom))
+            }
+
+            // Add support for Jvm Modules
+            pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+                dependencies {
+                    "implementation"(libs.findLibrary("koin.core").get())
+                }
+            }
+
+            /** Add support for Android modules (based on [AndroidBasePlugin]) */
+            pluginManager.withPlugin("com.android.base") {
+                dependencies {
+                    "implementation"(libs.findLibrary("koin.android").get())
+                    "androidTestImplementation"(platform(bom))
+                }
+            }
+        }
+    }
+} 
