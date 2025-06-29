924e7e20fc02c00013b3fe7705735e974d5558dc
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-29 10:17:29 +0700

    chore(build-config/convention): add Android feature convention plugin

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,32 @@
+import com.music.dzr.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "dzr.android.library")
+            apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+            apply(plugin = "dzr.koin")
+
+            dependencies {
+                "implementation"(project(":core:ui"))
+                "implementation"(project(":core:design-system"))
+
+                "implementation"(libs.findLibrary("androidx.lifecycle.runtime.compose").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.viewModel.compose").get())
+                "implementation"(libs.findLibrary("koin.androidx.compose").get())
+                "implementation"(libs.findLibrary("koin.androidx.compose.navigation").get())
+                "implementation"(libs.findLibrary("kotlinx.serialization.json").get())
+                "implementation"(libs.findLibrary("androidx.navigation.compose").get())
+
+                "testImplementation"(libs.findLibrary("androidx.navigation.testing").get())
+                "androidTestImplementation"(
+                    libs.findLibrary("androidx.lifecycle.runtime-testing").get()
+                )
+            }
+        }
+    }
+}
