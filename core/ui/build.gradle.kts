bb5f3e006fdc8588b96974387e8ef3050ddcf830
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-29 14:14:42 +0700

    chore(core/ui): apply AndroidLibrary*ConventionPlugin

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,31 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dzr.android.library)
+    alias(libs.plugins.dzr.android.library.compose)
 }
 
 android {
     namespace = "com.music.dzr.core.ui"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 
     packaging {
         resources.excludes.addAll(
@@ -38,17 +17,12 @@ android {
 }
 
 dependencies {
-    val composeBom = platform(libs.androidx.compose.bom)
-    implementation(composeBom)
     api(project(":core:design-system"))
     api(project(":core:model"))
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.coil.compose)
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.mockk.android)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
 }
