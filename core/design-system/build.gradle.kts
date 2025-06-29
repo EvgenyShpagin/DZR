2aa432f14f2410f4ba80fe7ab1a8dfca8e85755f
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-06-29 21:03:52 +0700

    chore(core/design-system): apply convention plugins

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -1,32 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.screenshot)
+    alias(libs.plugins.dzr.android.library)
+    alias(libs.plugins.dzr.android.library.compose)
+    alias(libs.plugins.dzr.screenshot)
 }
 
 android {
     namespace = "com.music.dzr.core.designsystem"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 
     packaging {
         resources.excludes.addAll(
@@ -36,13 +15,9 @@ android {
             )
         )
     }
-
-    experimentalProperties["android.experimental.enableScreenshotTest"] = true
 }
 
 dependencies {
-    val composeBom = platform(libs.androidx.compose.bom)
-    implementation(composeBom)
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.runtime)
@@ -51,14 +26,8 @@ dependencies {
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.material3.adaptive)
     api(libs.androidx.compose.material3.navigationSuite)
-    implementation(libs.androidx.ui.tooling.preview)
-    debugImplementation(libs.androidx.ui.tooling)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.mockk.android)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    screenshotTestImplementation(libs.screenshot.validation.api)
-    screenshotTestImplementation(libs.androidx.ui.tooling)
 }
