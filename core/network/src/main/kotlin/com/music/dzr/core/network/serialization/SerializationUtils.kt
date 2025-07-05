dcef4057429e880e8fa4396e9df90f5999d14a49
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-07-05 20:03:35 +0700

    chore: update Kotlin serialization to 1.9.0
    This commit updates the Kotlin serialization library from version 1.6.3 to 1.9.0.
    
    It also removes unnecessary `@OptIn(ExperimentalSerializationApi::class)` annotations from `SerializationUtils.kt` as the APIs are no longer experimental in the newer version.

--- core/network/src/main/kotlin/com/music/dzr/core/network/serialization/SerializationUtils.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/serialization/SerializationUtils.kt
@@ -1,6 +1,5 @@
 package com.music.dzr.core.network.serialization
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.serializer
@@ -11,7 +10,6 @@ import kotlinx.serialization.serializer
  *
  * @param rawValue The raw string value that could not be deserialized.
  */
-@OptIn(ExperimentalSerializationApi::class)
 internal fun KSerializer<*>.throwDeserializationException(rawValue: String): Nothing {
     throw SerializationException(
         "Couldn't deserialize '$rawValue' to type ${descriptor.serialName}"
@@ -22,7 +20,6 @@ internal fun KSerializer<*>.throwDeserializationException(rawValue: String): Not
  * An extension function for enums to get their serialized name based on the @SerialName annotation.
  * This provides a reusable and type-safe way to avoid string duplication.
  */
-@OptIn(ExperimentalSerializationApi::class)
 internal inline fun <reified T : Enum<T>> Enum<T>.getSerializedName(): String {
     return serializer<T>().descriptor.getElementName(ordinal)
 } 
