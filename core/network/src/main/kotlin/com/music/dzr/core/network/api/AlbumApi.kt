c9f15ff5c2067d138770bc5d1850be3eb1dd3fbe
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-07-09 13:59:01 +0700

    chore(core/network): add PermissionScope to AlbumApi

--- core/network/src/main/kotlin/com/music/dzr/core/network/api/AlbumApi.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/api/AlbumApi.kt
@@ -4,6 +4,7 @@ import com.music.dzr.core.network.model.album.Album
 import com.music.dzr.core.network.model.album.Albums
 import com.music.dzr.core.network.model.album.NewReleases
 import com.music.dzr.core.network.model.album.SavedAlbum
+import com.music.dzr.core.network.model.auth.PermissionScope
 import com.music.dzr.core.network.model.shared.NetworkResponse
 import com.music.dzr.core.network.model.shared.PaginatedList
 import com.music.dzr.core.network.model.album.AlbumTrack
