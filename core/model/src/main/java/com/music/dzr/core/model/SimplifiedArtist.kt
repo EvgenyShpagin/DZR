43b62c26a5fe49ecfb71f23f20f46b13c9877c10
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-07-31 12:31:50 +0700

    refactor(library/artist): rename `Artist` to `DetailedArtist` and extend core `Artist` model

--- core/model/src/main/java/com/music/dzr/core/model/SimplifiedArtist.kt
+++ core/model/src/main/java/com/music/dzr/core/model/SimplifiedArtist.kt
@@ -4,7 +4,7 @@ package com.music.dzr.core.model
  * Artist reference used in album and track contexts.
  */
 data class SimplifiedArtist(
-    val id: String,
-    val name: String,
-    val externalUrl: String
-)
+    override val id: String,
+    override val name: String,
+    override val externalUrl: String
+) : Artist()
--- library/artist/domain/src/main/kotlin/com/music/dzr/library/artist/domain/model/DetailedArtist.kt
+++ library/artist/domain/src/main/kotlin/com/music/dzr/library/artist/domain/model/DetailedArtist.kt
@@ -1,5 +1,6 @@
 package com.music.dzr.library.artist.domain.model
 
+import com.music.dzr.core.model.Artist
 import com.music.dzr.core.model.Image
 import com.music.dzr.core.model.MusicGenre
 import com.music.dzr.core.model.PopularityLevel
@@ -7,12 +8,12 @@ import com.music.dzr.core.model.PopularityLevel
 /**
  * Music artist with full profile information.
  */
-data class Artist(
-    val id: String,
-    val name: String,
+data class DetailedArtist(
+    override val id: String,
+    override val name: String,
+    override val externalUrl: String,
     val genres: List<MusicGenre>,
     val images: List<Image>,
-    val externalUrl: String,
     val followersCount: Int,
     val popularity: PopularityLevel
-)
+) : Artist()
