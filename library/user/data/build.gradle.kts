d2fcfb0bfc5644679c58f61e7a3ef2a7aaab7f91
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-01 19:49:39 +0700

    feat(library/user): add data module
    - Added `library:user:data` to `settings.gradle.kts`.
    - Created `.gitignore` for the new module.
    - Configured `build.gradle.kts` for the `library.user.data` module, including:
        - Android library, Koin and Kotlin serialization plugins.
        - Release build type configuration.
        - Dependencies on `kotlinx-serialization-json`, `retrofit-core`, and `retrofit-kotlin-serialization`.

---
+++ library/user/data/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.dzr.android.library)
+    alias(libs.plugins.dzr.koin)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+}
