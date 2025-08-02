78367e9fdf55573641c2eaaae0362f59a5722ac7
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-02 10:19:10 +0700

    refactor(core/network): make getSerializedName public

--- core/network/src/main/kotlin/com/music/dzr/core/network/serialization/SerializationUtils.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/serialization/SerializationUtils.kt
@@ -20,6 +20,6 @@ internal fun KSerializer<*>.throwDeserializationException(rawValue: String): Not
  * An extension function for enums to get their serialized name based on the @SerialName annotation.
  * This provides a reusable and type-safe way to avoid string duplication.
  */
-internal inline fun <reified T : Enum<T>> Enum<T>.getSerializedName(): String {
+inline fun <reified T : Enum<T>> Enum<T>.getSerializedName(): String {
     return serializer<T>().descriptor.getElementName(ordinal)
 } 
