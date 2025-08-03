43ae6e239cac4613065cfd0a87b5b2cf716eb887
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-03 16:08:51 +0700

    refactor(library/player): update package in PlayerApiTest for consistency

--- library/player/data/src/test/kotlin/com/music/dzr/library/player/data/remote/api/PlayerApiTest.kt
+++ library/player/data/src/test/kotlin/com/music/dzr/library/player/data/remote/api/PlayerApiTest.kt
@@ -1,9 +1,8 @@
-package com.music.dzr.library.player.data.remote
+package com.music.dzr.library.player.data.remote.api
 
 import com.music.dzr.core.network.api.createApi
 import com.music.dzr.core.network.api.enqueueEmptyResponse
 import com.music.dzr.core.network.api.enqueueResponseFromAssets
-import com.music.dzr.library.player.data.remote.api.PlayerApi
 import com.music.dzr.library.player.data.remote.dto.Offset
 import com.music.dzr.library.player.data.remote.dto.PlaybackOptions
 import com.music.dzr.library.player.data.remote.dto.RepeatMode
