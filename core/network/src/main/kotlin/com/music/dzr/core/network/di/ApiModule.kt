f51699704e570da2ffa63cc2b1f006cb19980b24
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-03 17:18:36 +0700

    refactor(core/network): remove deprecated API interfaces for better module organization
    Removed `AlbumApi`, `ArtistApi`, `PlayerApi`, `PlaylistApi`, and `TrackApi` from `core/network`.

--- core/network/src/main/kotlin/com/music/dzr/core/network/di/ApiModule.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/di/ApiModule.kt
@@ -1,15 +1,9 @@
 package com.music.dzr.core.network.di
 
 import com.music.dzr.core.network.BuildConfig
-import com.music.dzr.core.network.api.AlbumApi
-import com.music.dzr.core.network.api.ArtistApi
 import com.music.dzr.core.network.api.AuthApi
 import com.music.dzr.core.network.api.MarketApi
-import com.music.dzr.core.network.api.PlayerApi
-import com.music.dzr.core.network.api.PlaylistApi
 import com.music.dzr.core.network.api.SearchApi
-import com.music.dzr.core.network.api.TrackApi
-import com.music.dzr.core.network.api.UserApi
 import com.music.dzr.core.network.util.AuthorizationUrlBuilder
 import org.koin.core.definition.KoinDefinition
 import org.koin.core.module.Module
@@ -23,14 +17,8 @@ internal val apiModule = module {
     single { AuthorizationUrlBuilder(BuildConfig.SPOTIFY_CLIENT_ID) }
 
     singleApi<AuthApi>(AUTH_RETROFIT)
-
-    singleApi<ArtistApi>(API_RETROFIT)
     singleApi<MarketApi>(API_RETROFIT)
-    singleApi<PlayerApi>(API_RETROFIT)
-    singleApi<PlaylistApi>(API_RETROFIT)
     singleApi<SearchApi>(API_RETROFIT)
-    singleApi<TrackApi>(API_RETROFIT)
-    singleApi<UserApi>(API_RETROFIT)
 }
 
 private inline fun <reified T> Module.singleApi(retrofitName: String): KoinDefinition<T> {
