8c63f57e0c3e7a77da2e10826ebc850a97b7689c
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-05 14:24:53 +0700

    feat(core/auth/domain): add PermissionScope to represent user permission scopes
    - Introduced `PermissionScope` enum to model various permission levels for user data access and actions.
    - Improves clarity and modularity in the authentication domain.

---
+++ core/auth/domain/src/main/kotlin/com/music/dzr/core/auth/domain/model/PermissionScope.kt
@@ -0,0 +1,98 @@
+package com.music.dzr.core.auth.domain.model
+
+/**
+ * Represents various permission scopes that can be requested
+ * for accessing user data and performing actions on behalf of the user.
+ */
+enum class PermissionScope {
+
+    /**
+     * Write access to user-provided images.
+     */
+    UgcImageUpload,
+
+    /**
+     * Read access to a user's player state.
+     */
+    UserReadPlaybackState,
+
+    /**
+     * Write access to a user's playback state
+     */
+    UserModifyPlaybackState,
+
+    /**
+     * Read access to a user's currently playing content.
+     */
+    UserReadCurrentlyPlaying,
+
+    /**
+     * 	Remote control playback of Spotify.
+     */
+    AppRemoteControl,
+
+    /**
+     * 	Control playback of a Spotify track. The user must have a Spotify Premium account.
+     */
+    Streaming,
+
+    /**
+     * Read access to user's private playlists.
+     */
+    PlaylistReadPrivate,
+
+    /**
+     * Include collaborative playlists when requesting a user's playlists.
+     */
+    PlaylistReadCollaborative,
+
+    /**
+     * Write access to a user's private playlists.
+     */
+    PlaylistModifyPrivate,
+
+    /**
+     * Write access to a user's public playlists.
+     */
+    PlaylistModifyPublic,
+
+    /**
+     * Write/delete access to the list of artists and other users that the user follows.
+     */
+    UserFollowModify,
+
+    /**
+     * Read access to the list of artists and other users that the user follows.
+     */
+    UserFollowRead,
+
+    /**
+     * Read access to a user's top artists and tracks.
+     */
+    UserTopRead,
+
+    /**
+     * Read access to a user's recently played tracks.
+     */
+    UserReadRecentlyPlayed,
+
+    /**
+     * Write/delete access to a user's "Your Music" library.
+     */
+    UserLibraryModify,
+
+    /**
+     * Read access to a user's library.
+     */
+    UserLibraryRead,
+
+    /**
+     * Read access to user's email address.
+     */
+    UserReadEmail,
+
+    /**
+     * Read access to user's subscription details (type of user account).
+     */
+    UserReadPrivate,
+}
