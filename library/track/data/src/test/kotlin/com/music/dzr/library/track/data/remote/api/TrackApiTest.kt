eb441303073781e46eed919e8168d22a4337d998
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-13 09:37:39 +0700

    refactor(library/track/data): move api test to its package

--- library/track/data/src/test/kotlin/com/music/dzr/library/track/data/remote/api/TrackApiTest.kt
+++ library/track/data/src/test/kotlin/com/music/dzr/library/track/data/remote/api/TrackApiTest.kt
@@ -1,4 +1,4 @@
-package com.music.dzr.library.track.data.remote
+package com.music.dzr.library.track.data.remote.api
 
 import com.music.dzr.core.network.test.createApi
 import com.music.dzr.core.network.test.enqueueEmptyResponse
