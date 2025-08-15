beb4358ac1cfd7d8e09b9e84b7be2652c6e0ff60
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-15 11:08:40 +0700

    feat(core/model): create base Page interface

---
+++ core/model/src/main/java/com/music/dzr/core/model/Page.kt
@@ -0,0 +1,13 @@
+package com.music.dzr.core.model
+
+/**
+ * Represents a page of items in a paginated data set.
+ *
+ * @param T the type of items contained in the page.
+ * @property items the list of items for the current page.
+ * @property hasMore `true` if more items are available to load, `false` otherwise.
+ */
+sealed interface Page<T> {
+    val items: List<T>
+    val hasMore: Boolean
+}
