b783bd1dcd57476f20ad7852ab7a9641a88b658c
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-20 11:03:58 +0700

    feat(core/data): add Markets mapper

---
+++ core/data/src/main/kotlin/com/music/dzr/core/data/mapper/MarketMapper.kt
@@ -0,0 +1,14 @@
+package com.music.dzr.core.data.mapper
+
+import com.music.dzr.core.data.remote.dto.Markets
+import com.music.dzr.core.model.Market as DomainMarket
+
+
+fun Markets.toDomain(): List<DomainMarket> {
+    return list.map { code -> DomainMarket(code) }
+}
+
+fun List<DomainMarket>.toNetwork(): Markets {
+    val codes = map { market -> market.code }
+    return Markets(codes)
+}
