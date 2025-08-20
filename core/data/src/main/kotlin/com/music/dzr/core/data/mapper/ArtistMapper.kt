50836295b7cb3869cbe166235dcfb38bb95acbf4
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-20 13:19:56 +0700

    feat(core/data): add Artist mappers

---
+++ core/data/src/main/kotlin/com/music/dzr/core/data/mapper/ArtistMapper.kt
@@ -0,0 +1,29 @@
+package com.music.dzr.core.data.mapper
+
+import com.music.dzr.core.model.MusicGenre
+import com.music.dzr.core.model.PopularityLevel
+import com.music.dzr.core.model.DetailedArtist as DomainArtist
+import com.music.dzr.core.model.SimplifiedArtist as DomainSimplifiedArtist
+import com.music.dzr.core.network.dto.Artist as NetworkArtist
+import com.music.dzr.core.network.dto.SimplifiedArtist as NetworkSimplifiedArtist
+
+
+fun NetworkSimplifiedArtist.toDomain(): DomainSimplifiedArtist {
+    return DomainSimplifiedArtist(
+        id = id,
+        name = name,
+        externalUrl = externalUrls.spotify
+    )
+}
+
+fun NetworkArtist.toDomain(): DomainArtist {
+    return DomainArtist(
+        id = id,
+        name = name,
+        externalUrl = externalUrls.spotify,
+        genres = genres.map { MusicGenre.parse(it) },
+        images = images.map { it.toDomain() },
+        followersCount = followers.total,
+        popularity = PopularityLevel.parse(popularity)
+    )
+}
