ab6379a0c841207080e7c20a5da385429bae53c0
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-22 13:46:57 +0700

    feat(library/user/domain): create SavedTrack typealias

---
+++ library/track/domain/src/main/kotlin/com/music/dzr/library/track/domain/model/SavedTrack.kt
@@ -0,0 +1,6 @@
+package com.music.dzr.library.track.domain.model
+
+import com.music.dzr.core.model.SavedContent
+import com.music.dzr.core.model.Track
+
+internal typealias SavedTrack = SavedContent<Track>
