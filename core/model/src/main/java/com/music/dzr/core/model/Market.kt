d2951f13534f54df6b579166370ed8bbeefbb444
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-28 19:14:47 +0700

    refactor(core/model): add Market.Unspecified property

--- core/model/src/main/java/com/music/dzr/core/model/Market.kt
+++ core/model/src/main/java/com/music/dzr/core/model/Market.kt
@@ -8,12 +8,21 @@ package com.music.dzr.core.model
  */
 @JvmInline
 value class Market(val code: String) {
-    
+
     init {
         require(code.isNotBlank()) { "Market code cannot be blank" }
         require(code.length == 2) { "Market code must be 2 characters (ISO 3166-1 alpha-2)" }
         require(code.all { it.isUpperCase() }) { "Market code must be uppercase" }
     }
-    
+
     override fun toString(): String = code
+
+    companion object {
+        /**
+         * A special value representing an unspecified market.
+         *
+         * This must be mapped to a null or omitted parameter in any external API call.
+         */
+        val Unspecified = Market("ZZ")
+    }
 }
