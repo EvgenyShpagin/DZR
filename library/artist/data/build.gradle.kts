4b938e642338a390eb20bdce120aa3635aee8903
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-01 09:44:04 +0700

    chore(library/artist): add dependencies

--- library/artist/data/build.gradle.kts
+++ library/artist/data/build.gradle.kts
@@ -11,3 +11,9 @@ android {
         }
     }
 }
+
+dependencies {
+    implementation(projects.dzr.core.data)
+    implementation(projects.dzr.library.artist.domain)
+    testImplementation(testFixtures(projects.dzr.core.data))
+}
