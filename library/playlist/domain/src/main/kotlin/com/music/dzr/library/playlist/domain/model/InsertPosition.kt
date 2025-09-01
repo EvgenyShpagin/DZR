2148837abe038da168010399070ce53c4ff3a98b
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-01 09:54:54 +0700

    chore(library/playlist/domain): add KDoc to InsertPosition

--- library/playlist/domain/src/main/kotlin/com/music/dzr/library/playlist/domain/model/InsertPosition.kt
+++ library/playlist/domain/src/main/kotlin/com/music/dzr/library/playlist/domain/model/InsertPosition.kt
@@ -1,5 +1,11 @@
 package com.music.dzr.library.playlist.domain.model
 
+/**
+ * Defines the position where a new item should be inserted.
+ *
+ * Use [Append] to add an item to the end of a collection,
+ * or [At] to insert it at the given index.
+ */
 sealed interface InsertPosition {
     data object Append : InsertPosition
     data class At(val index: Int) : InsertPosition
