1433e00f481e07339afe79368135b087200c06ec
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-04 18:21:08 +0700

    refactor(library/album/domain): update DetailedAlbum to use Availability

--- library/album/domain/src/main/kotlin/com/music/dzr/library/album/domain/model/DetailedAlbum.kt
+++ library/album/domain/src/main/kotlin/com/music/dzr/library/album/domain/model/DetailedAlbum.kt
@@ -1,11 +1,10 @@
 package com.music.dzr.library.album.domain.model
 
 import com.music.dzr.core.model.Album
-import com.music.dzr.core.model.ContentRestriction
+import com.music.dzr.core.model.AlbumAvailability
 import com.music.dzr.core.model.Copyright
 import com.music.dzr.core.model.ExternalIdentifiers
 import com.music.dzr.core.model.Image
-import com.music.dzr.core.model.Market
 import com.music.dzr.core.model.MusicGenre
 import com.music.dzr.core.model.PopularityLevel
 import com.music.dzr.core.model.ReleaseDate
@@ -17,15 +16,14 @@ import com.music.dzr.core.model.SimplifiedArtist
  */
 data class DetailedAlbum(
     override val id: String,
+    override val availability: AlbumAvailability,
     override val name: String,
     override val releaseType: ReleaseType,
     override val totalTracks: Int,
     override val images: List<Image>,
     override val releaseDate: ReleaseDate,
-    override val availableMarkets: List<Market>,
     override val externalUrl: String,
     override val artists: List<SimplifiedArtist>,
-    override val restriction: ContentRestriction?,
     val tracks: List<TrackOnAlbum>,
     val copyrights: List<Copyright>,
     val externalIds: ExternalIdentifiers,
