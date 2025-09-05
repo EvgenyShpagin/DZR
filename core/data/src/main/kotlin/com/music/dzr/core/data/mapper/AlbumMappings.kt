7697ab2cd37e0e6e36e5813261ce9e25aa027782
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-05 09:30:26 +0700

    refactor(core/data): update mappings using Availability

--- core/data/src/main/kotlin/com/music/dzr/core/data/mapper/AlbumMappings.kt
+++ core/data/src/main/kotlin/com/music/dzr/core/data/mapper/AlbumMappings.kt
@@ -1,20 +1,27 @@
 package com.music.dzr.core.data.mapper
 
-import com.music.dzr.core.data.remote.dto.Markets as NetworkMarkets
+import com.music.dzr.core.model.AlbumAvailability
+import com.music.dzr.core.model.Availability
 import com.music.dzr.core.model.AlbumOnTrack as DomainAlbumOnTrack
 import com.music.dzr.core.network.dto.TrackAlbum as NetworkAlbumOnTrack
 
 fun NetworkAlbumOnTrack.toDomain(): DomainAlbumOnTrack {
     return DomainAlbumOnTrack(
         id = id,
+        availability = availabilityFromNetwork(),
         name = name,
         releaseType = albumType.toDomain(),
         totalTracks = totalTracks,
         images = images.map { it.toDomain() },
         releaseDate = releaseDate.toDomain(),
-        availableMarkets = NetworkMarkets(availableMarkets).toDomain(),
         externalUrl = externalUrls.spotify,
         artists = artists.map { it.toDomain() },
-        restriction = restrictions?.toDomain()
     )
+}
+
+private fun NetworkAlbumOnTrack.availabilityFromNetwork(): AlbumAvailability {
+    return Availability.fromNetwork(
+        isPlayable = isPlayable,
+        restrictions = restrictions
+    ) as AlbumAvailability
 }
--- core/data/src/main/kotlin/com/music/dzr/core/data/mapper/DetailedTrackMappings.kt
+++ core/data/src/main/kotlin/com/music/dzr/core/data/mapper/DetailedTrackMappings.kt
@@ -1,6 +1,8 @@
 package com.music.dzr.core.data.mapper
 
+import com.music.dzr.core.model.Availability
 import com.music.dzr.core.model.PopularityLevel
+import com.music.dzr.core.model.TrackAvailability
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 import com.music.dzr.core.model.DetailedTrack as DomainTrack
@@ -9,18 +11,25 @@ import com.music.dzr.core.network.dto.Track as NetworkTrack
 fun NetworkTrack.toDomain(): DomainTrack {
     return DomainTrack(
         id = id,
+        availability = availabilityFromNetwork(),
         name = name,
         artists = artists.map { it.toDomain() },
         duration = durationMs.toDuration(DurationUnit.MILLISECONDS),
         trackNumber = trackNumber,
         discNumber = discNumber,
         isExplicit = explicit,
-        isPlayable = isPlayable == true,
-        isLocal = isLocal,
         externalUrl = externalUrls.spotify,
-        restriction = restrictions?.toDomain(),
         album = album.toDomain(),
         popularity = PopularityLevel.fromNetwork(popularity),
         externalIds = externalIds.toDomain()
     )
+}
+
+private fun NetworkTrack.availabilityFromNetwork(): TrackAvailability {
+    return Availability.fromNetwork(
+        isPlayable = isPlayable,
+        restrictions = restrictions,
+        isLocal = isLocal,
+        linkedFrom = linkedFrom
+    ) as TrackAvailability
 }
