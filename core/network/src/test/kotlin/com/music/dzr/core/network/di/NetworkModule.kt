f5a3bf3cf0c86e9c6dc289c3e66a5f249030feb8
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-07 19:37:30 +0700

    test(core/network): update di test

--- core/network/src/test/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
+++ core/network/src/test/kotlin/com/music/dzr/core/network/di/NetworkModule.kt
@@ -1,7 +1,5 @@
 package com.music.dzr.core.network.di
 
-import com.music.dzr.core.auth.domain.repository.AuthTokenRepository
-import io.mockk.mockk
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.dsl.module
 import org.koin.test.verify.verify
@@ -12,15 +10,9 @@ class NetworkModuleTest {
 
     @Test
     fun verifiesDependencyGraphSuccessfully() {
-        // Arrange: The network module requires a TokenRepository.
-        // We provide a relaxed mock to satisfy this dependency for verification.
-        val mockTokenRepositoryModule = module {
-            single<AuthTokenRepository> { mockk(relaxed = true) }
-        }
-
         // Act & Assert: Combine the real network module with the mock and run Koin's verification.
         module {
-            includes(networkModule, mockTokenRepositoryModule)
+            includes(networkModule)
         }.verify()
     }
 }
