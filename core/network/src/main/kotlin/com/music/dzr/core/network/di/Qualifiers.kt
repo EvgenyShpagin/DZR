0edadb5bad306193f2d2fc5668109d4c20cd23c8
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-07 10:21:54 +0700

    refactor(core/network): make Api Retrofit public

--- core/network/src/main/kotlin/com/music/dzr/core/network/di/Qualifiers.kt
+++ core/network/src/main/kotlin/com/music/dzr/core/network/di/Qualifiers.kt
@@ -2,7 +2,7 @@ package com.music.dzr.core.network.di
 
 import org.koin.core.qualifier.named
 
-internal val ApiRetrofitQualifier = named("ApiRetrofit")
+val ApiRetrofitQualifier = named("ApiRetrofit")
 internal val AuthRetrofitQualifier = named("AuthRetrofit")
 
 internal val ApiClientQualifier = named("ApiOkHttpClient")
