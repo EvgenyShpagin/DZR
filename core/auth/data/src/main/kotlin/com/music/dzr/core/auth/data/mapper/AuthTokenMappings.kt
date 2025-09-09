1fa8ab4a75cdc5647d4f99f2f449ee917f392907
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-09 14:24:43 +0700

    chore(core/auth/data): add domain Token toNetwork mapper

--- core/auth/data/src/main/kotlin/com/music/dzr/core/auth/data/mapper/AuthTokenMappings.kt
+++ core/auth/data/src/main/kotlin/com/music/dzr/core/auth/data/mapper/AuthTokenMappings.kt
@@ -1,6 +1,7 @@
 package com.music.dzr.core.auth.data.mapper
 
 import com.music.dzr.core.auth.domain.model.AuthScope
+import com.music.dzr.core.auth.domain.model.AuthScope.Companion.join
 import com.music.dzr.core.auth.data.remote.dto.AuthToken as NetworkToken
 import com.music.dzr.core.auth.domain.model.AuthToken as DomainToken
 
@@ -11,5 +12,15 @@ internal fun NetworkToken.toDomain(): DomainToken {
         expiresInSeconds = expiresIn,
         refreshToken = refreshToken,
         scopes = scope?.let { AuthScope.parse(it) } ?: emptyList()
+    )
+}
+
+internal fun DomainToken.toNetwork(): NetworkToken {
+    return NetworkToken(
+        accessToken = accessToken,
+        tokenType = tokenType,
+        expiresIn = expiresInSeconds,
+        refreshToken = refreshToken,
+        scope = scopes.join()
     )
 }
