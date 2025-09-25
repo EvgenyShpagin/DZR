a10009a96bcb7721d3dc995a80bbc80e87ac6182
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-25 10:15:55 +0700

    chore(core/auth/data): add datasource for auth params

---
+++ core/auth/data/src/main/kotlin/com/music/dzr/core/auth/data/local/source/AuthSessionLocalDataSource.kt
@@ -0,0 +1,45 @@
+package com.music.dzr.core.auth.data.local.source
+
+import com.music.dzr.core.auth.data.local.error.AuthStorageError
+import com.music.dzr.core.auth.data.local.model.AuthSession
+import com.music.dzr.core.data.error.StorageError
+import com.music.dzr.core.result.Result
+
+/**
+ * A data source for managing a short-lived authorization session, which includes
+ * parameters like the PKCE verifier and OAuth state.
+ *
+ * Persisting these values is crucial for the authorization flow to withstand
+ * process death, ensuring a seamless user experience.
+ *
+ * Implementations of this interface must prioritize security by storing data
+ * securely and preventing any leakage of sensitive information into logs.
+ */
+internal interface AuthSessionLocalDataSource {
+
+    /**
+     * Saves the provided authorization session.
+     *
+     * @param authSession The [AuthSession] containing the authorization data to be stored.
+     * @return [Result.Success] on successful save, or [Result.Failure]
+     *  with an [AuthStorageError] or a base [StorageError] if an error occurs.
+     */
+    suspend fun save(authSession: AuthSession): Result<Unit, StorageError>
+
+    /**
+     * Retrieves the current authorization session.
+     *
+     * @return [Result.Success] with the [AuthSession] if found, or [Result.Failure]
+     *  with an [StorageError.NotFound] if no session is stored, or another
+     *  [AuthStorageError]/[StorageError] if a different error occurs.
+     */
+    suspend fun get(): Result<AuthSession, StorageError>
+
+    /**
+     * Clears any stored authorization session data.
+     *
+     * @return [Result.Success] on successful deletion, or [Result.Failure]
+     *  with an [AuthStorageError] or a base [StorageError] if an error occurs.
+     */
+    suspend fun clear(): Result<Unit, StorageError>
+}
