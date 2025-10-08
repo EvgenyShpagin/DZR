df044fb4795959a4b0bf56c8d849dd71f0ae78da
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-10-08 21:15:48 +0700

    chore(core/data): add core:storage dep

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -18,6 +18,9 @@ android {
 dependencies {
     api(projects.dzr.core.model)
     api(projects.dzr.core.common)
+    // core:storage could be provided by a convention plugin,
+    // but added directly as it's the only storage-related dependency for now.
+    api(projects.dzr.core.storage)
 
     testFixturesApi(projects.dzr.core.network)
 }
