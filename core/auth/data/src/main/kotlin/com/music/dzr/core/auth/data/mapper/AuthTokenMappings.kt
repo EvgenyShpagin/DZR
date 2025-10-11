f67c16af818bdfbe42ebe2a02e6bd7e4e3a8da59
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-10-11 13:20:19 +0700

    fix(core/auth/data): add missed mappings

--- core/auth/data/src/main/kotlin/com/music/dzr/core/auth/data/mapper/AuthTokenMappings.kt
+++ core/auth/data/src/main/kotlin/com/music/dzr/core/auth/data/mapper/AuthTokenMappings.kt
@@ -1,7 +1,9 @@
 package com.music.dzr.core.auth.data.mapper
 
+import com.music.dzr.core.auth.data.local.model.authToken
 import com.music.dzr.core.auth.domain.model.AuthScope
 import com.music.dzr.core.auth.domain.model.AuthScope.Companion.join
+import com.music.dzr.core.auth.data.local.model.AuthToken as LocalToken
 import com.music.dzr.core.auth.data.remote.dto.AuthToken as NetworkToken
 import com.music.dzr.core.auth.domain.model.AuthToken as DomainToken
 
@@ -23,4 +25,29 @@ internal fun DomainToken.toNetwork(): NetworkToken {
         refreshToken = refreshToken,
         scope = scopes.join()
     )
+}
+
+internal fun LocalToken.toDomain(): DomainToken {
+    val refresh = if (this.hasRefreshToken()) this.refreshToken else null
+    val scopes = if (this.hasScope()) AuthScope.parse(this.scope) else emptyList()
+    return DomainToken(
+        accessToken = this.accessToken,
+        tokenType = this.tokenType,
+        expiresInSeconds = this.expiresIn,
+        refreshToken = refresh,
+        scopes = scopes
+    )
+}
+
+internal fun DomainToken.toLocal(): LocalToken = authToken {
+    val domain = this@toLocal
+    accessToken = domain.accessToken
+    tokenType = domain.tokenType
+    expiresIn = domain.expiresInSeconds
+
+    domain.refreshToken?.let { refreshToken = it }
+
+    if (domain.scopes.isNotEmpty()) {
+        scope = domain.scopes.join()
+    }
 }
