e49dfbf327c195b714468cef78a8baf85dd675f8
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-10-11 18:16:13 +0700

    chore(core/auth/data): add core:data dep

--- core/auth/data/build.gradle.kts
+++ core/auth/data/build.gradle.kts
@@ -17,6 +17,7 @@ android {
 
 dependencies {
     implementation(projects.core.auth.domain)
+    implementation(projects.dzr.core.data)
     implementation(projects.dzr.core.storage)
     implementation(libs.okhttp.logging)
 }
