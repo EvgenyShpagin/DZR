0e4c07869213bf5ff7bff136a88013cc0457d20c
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-10-15 18:35:42 +0700

    docs(library/auth/data): remove KDoc from datasource impl

--- library/album/data/src/main/kotlin/com/music/dzr/library/album/data/remote/source/AlbumRemoteDataSourceImpl.kt
+++ library/album/data/src/main/kotlin/com/music/dzr/library/album/data/remote/source/AlbumRemoteDataSourceImpl.kt
@@ -12,16 +12,10 @@ internal class AlbumRemoteDataSourceImpl(
     private val albumApi: AlbumApi
 ) : AlbumRemoteDataSource {
 
-    /**
-     * Retrieves detailed information about a single album.
-     */
     override suspend fun getAlbum(id: String, market: String?): NetworkResponse<Album> {
         return albumApi.getAlbum(id, market)
     }
 
-    /**
-     * Retrieves detailed information for multiple albums.
-     */
     override suspend fun getMultipleAlbums(
         ids: List<String>,
         market: String?
@@ -30,9 +24,6 @@ internal class AlbumRemoteDataSourceImpl(
         return albumApi.getMultipleAlbums(csv, market)
     }
 
-    /**
-     * Retrieves a paginated list of tracks for a given album.
-     */
     override suspend fun getAlbumTracks(
         id: String,
         market: String?,
@@ -42,9 +33,6 @@ internal class AlbumRemoteDataSourceImpl(
         return albumApi.getAlbumTracks(id, market, limit, offset)
     }
 
-    /**
-     * Retrieves a paginated list of albums saved in the current user’s library.
-     */
     override suspend fun getUserSavedAlbums(
         limit: Int?,
         offset: Int?,
@@ -53,17 +41,11 @@ internal class AlbumRemoteDataSourceImpl(
         return albumApi.getUserSavedAlbums(limit, offset, market)
     }
 
-    /**
-     * Save one or more albums to the current user’s library.
-     */
     override suspend fun saveAlbumsForUser(ids: List<String>): NetworkResponse<Unit> {
         val csv = ids.joinToString(",")
         return albumApi.saveAlbumsForUser(ids = csv)
     }
 
-    /**
-     * Remove one or more albums from the current user’s library.
-     */
     override suspend fun removeAlbumsForUser(ids: List<String>): NetworkResponse<Unit> {
         val csv = ids.joinToString(",")
         return albumApi.removeAlbumsForUser(ids = csv)
