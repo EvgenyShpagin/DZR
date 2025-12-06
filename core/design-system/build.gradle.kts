8e6911d88c2f23ad34257c7e5bd533b5e19535f2
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-12-06 22:45:26 +0700

    refactor(core/design-system): use local fonts

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.runtime)
-    api(libs.androidx.ui.text.google.fonts)
     api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.material3.adaptive)
--- core/design-system/src/main/kotlin/com/music/dzr/core/designsystem/theme/Type.kt
+++ core/design-system/src/main/kotlin/com/music/dzr/core/designsystem/theme/Type.kt
@@ -1,47 +1,43 @@
 package com.music.dzr.core.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.music.dzr.core.designsystem.R
 
-private val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.core_design_system_com_google_android_gms_fonts_certs
-)
 
-private val fontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Oswald"),
-        fontProvider = provider,
-    )
+private val Oswald = FontFamily(
+    Font(R.font.oswald_extra_light, FontWeight.ExtraLight),
+    Font(R.font.oswald_light, FontWeight.Light),
+    Font(R.font.oswald_regular, FontWeight.Normal),
+    Font(R.font.oswald_medium, FontWeight.Medium),
+    Font(R.font.oswald_semi_bold, FontWeight.SemiBold),
+    Font(R.font.oswald_bold, FontWeight.Bold)
 )
 
 private val baseline = Typography()
 
 internal val DzrTypography = Typography(
     displayLarge = baseline.displayLarge.copy(
-        fontFamily = fontFamily,
+        fontFamily = Oswald,
         fontWeight = FontWeight.Bold
     ),
     displayMedium = baseline.displayMedium.copy(
-        fontFamily = fontFamily,
+        fontFamily = Oswald,
         fontWeight = FontWeight.Medium
     ),
-    displaySmall = baseline.displaySmall.copy(fontFamily = fontFamily),
-    headlineLarge = baseline.headlineLarge.copy(fontFamily = fontFamily),
-    headlineMedium = baseline.headlineMedium.copy(fontFamily = fontFamily),
-    headlineSmall = baseline.headlineSmall.copy(fontFamily = fontFamily),
-    titleLarge = baseline.titleLarge.copy(fontFamily = fontFamily),
-    titleMedium = baseline.titleMedium.copy(fontFamily = fontFamily),
-    titleSmall = baseline.titleSmall.copy(fontFamily = fontFamily),
-    bodyLarge = baseline.bodyLarge.copy(fontFamily = fontFamily),
-    bodyMedium = baseline.bodyMedium.copy(fontFamily = fontFamily),
-    bodySmall = baseline.bodySmall.copy(fontFamily = fontFamily),
-    labelLarge = baseline.labelLarge.copy(fontFamily = fontFamily),
-    labelMedium = baseline.labelMedium.copy(fontFamily = fontFamily),
-    labelSmall = baseline.labelSmall.copy(fontFamily = fontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = Oswald),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = Oswald),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = Oswald),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = Oswald),
+    titleLarge = baseline.titleLarge.copy(fontFamily = Oswald),
+    titleMedium = baseline.titleMedium.copy(fontFamily = Oswald, fontWeight = FontWeight.Medium),
+    titleSmall = baseline.titleSmall.copy(fontFamily = Oswald),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = Oswald),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = Oswald),
+    bodySmall = baseline.bodySmall.copy(fontFamily = Oswald),
+    labelLarge = baseline.labelLarge.copy(fontFamily = Oswald),
+    labelMedium = baseline.labelMedium.copy(fontFamily = Oswald),
+    labelSmall = baseline.labelSmall.copy(fontFamily = Oswald),
 )
