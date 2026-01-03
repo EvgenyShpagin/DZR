dfc870fcb3856c1f35917e7c602adb2f1adf5e4a
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-01-03 18:05:07 +0700

    test(feature/auth): add login button tests

---
+++ feature/auth/src/screenshotTest/kotlin/com/music/dzr/feature/auth/ui/screen/welcome/LoginButtonTest.kt
@@ -0,0 +1,15 @@
+package com.music.dzr.feature.auth.ui.screen.welcome
+
+import androidx.compose.runtime.Composable
+import com.android.tools.screenshot.PreviewTest
+import com.music.dzr.core.designsystem.component.PreviewLightDarkAndFontScale
+import com.music.dzr.core.designsystem.theme.DzrTheme
+
+@PreviewTest
+@PreviewLightDarkAndFontScale
+@Composable
+private fun LoginButton_Preview() {
+    DzrTheme {
+        LoginButton(onClick = {})
+    }
+}
