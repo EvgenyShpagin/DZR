31ff093d0be3aeab3fe9fabddec95af7ec4fd59e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-01-07 18:56:59 +0700

    refactor(feature/auth): add welcome preview data

---
+++ feature/auth/src/debug/kotlin/com/music/dzr/feature/auth/ui/screen/welcome/WelcomePreviewData.kt
@@ -0,0 +1,12 @@
+package com.music.dzr.feature.auth.ui.screen.welcome
+
+import androidx.compose.ui.graphics.Color
+
+internal val previewCoverMarquees = listOf(
+    WelcomeCoverMarqueeItemUiState("", Color.Blue),
+    WelcomeCoverMarqueeItemUiState("", Color.Cyan),
+    WelcomeCoverMarqueeItemUiState("", Color.Red),
+    WelcomeCoverMarqueeItemUiState("", Color.Magenta),
+    WelcomeCoverMarqueeItemUiState("", Color.Yellow),
+    WelcomeCoverMarqueeItemUiState("", Color.Green),
+)
