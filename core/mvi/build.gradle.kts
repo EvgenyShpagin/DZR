838cb2fce0965c6531b227e36f2c0c25a9967be9
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-01-17 19:21:36 +0700

    fix(core/mvi): add android block in build.gradle.kts

--- core/mvi/build.gradle.kts
+++ core/mvi/build.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     alias(libs.plugins.dzr.android.library)
 }
 
+android {
+    namespace = "com.music.dzr.core.mvi"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
 dependencies {
     api(libs.kotlinx.coroutines.core)
     api(libs.androidx.lifecycle.viewModel.ktx)
