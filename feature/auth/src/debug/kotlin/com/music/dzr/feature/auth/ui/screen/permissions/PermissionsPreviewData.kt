9ff3c7fd860209ae228047c19e6f95f438bdee61
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-01-18 09:35:50 +0700

    chore(feature/auth): take real permission in preview

--- feature/auth/src/debug/kotlin/com/music/dzr/feature/auth/ui/screen/permissions/PermissionsPreviewData.kt
+++ feature/auth/src/debug/kotlin/com/music/dzr/feature/auth/ui/screen/permissions/PermissionsPreviewData.kt
@@ -1,32 +1,8 @@
 package com.music.dzr.feature.auth.ui.screen.permissions
 
-import com.music.dzr.core.designsystem.icon.DzrIcons
-import com.music.dzr.feature.auth.R
+import com.music.dzr.core.auth.domain.model.PermissionScope
 
-internal val previewPermissions: List<PermissionUiState> = listOf(
-    PermissionUiState(
-        titleRes = R.string.feature_auth_permissions_account_type_and_premium_status,
-        imageVector = DzrIcons.AccountCircle,
-        isGranted = true
-    ),
-    PermissionUiState(
-        titleRes = R.string.feature_auth_permissions_email_address,
-        imageVector = DzrIcons.AlternateEmail,
-        isGranted = false
-    ),
-    PermissionUiState(
-        titleRes = R.string.feature_auth_permissions_music_streaming,
-        imageVector = DzrIcons.PlayCircle,
-        isGranted = true
-    ),
-    PermissionUiState(
-        titleRes = R.string.feature_auth_permissions_read_playback_activity,
-        imageVector = DzrIcons.History,
-        isGranted = false
-    ),
-    PermissionUiState(
-        titleRes = R.string.feature_auth_permissions_see_whats_playing,
-        imageVector = DzrIcons.QueueMusic,
-        isGranted = true
-    )
-)
+internal val previewPermissions: List<PermissionUiState> =
+    PermissionScope.entries.mapIndexed { index, permission ->
+        permission.toUiState(isGranted = index % 2 == 0)
+    }
