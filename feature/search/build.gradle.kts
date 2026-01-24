146a2c0caa2ca48ef6012e778ed1c30999d590b2
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-01-24 10:06:12 +0700

    chore(feature/search): remove .pro files

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -8,10 +8,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
