3ebbcf30b426815a57861361c3583b1c34c14a96
EvgenyShpagin <shpagin.from.usa@gmail.com>
2026-04-12 11:23:19 +0700

    chore(feature/auth): extend preview cover marquees

--- feature/auth/src/debug/kotlin/com/music/dzr/feature/auth/ui/screen/welcome/WelcomePreviewData.kt
+++ feature/auth/src/debug/kotlin/com/music/dzr/feature/auth/ui/screen/welcome/WelcomePreviewData.kt
@@ -3,10 +3,14 @@ package com.music.dzr.feature.auth.ui.screen.welcome
 import androidx.compose.ui.graphics.Color
 
 internal val previewCoverMarquees = listOf(
-    WelcomeCoverMarqueeItemUiState("", Color.Blue),
-    WelcomeCoverMarqueeItemUiState("", Color.Cyan),
-    WelcomeCoverMarqueeItemUiState("", Color.Red),
-    WelcomeCoverMarqueeItemUiState("", Color.Magenta),
-    WelcomeCoverMarqueeItemUiState("", Color.Yellow),
-    WelcomeCoverMarqueeItemUiState("", Color.Green),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFF36D6B5)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFFFF7B7B)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFF9D7AEA)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFF5B8DEF)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFFFDA4AF)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFF34D399)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFFFBBF24)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFF22D3EE)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFFFB923C)),
+    WelcomeCoverMarqueeItemUiState("", Color(0xFFF472B6)),
 )
